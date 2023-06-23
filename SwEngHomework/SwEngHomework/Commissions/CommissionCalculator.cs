5080fd71d48d9ec33c74d5ee9b122133fff9093e
Rodney Farris <rodney.farris@gocolonial.com>
2023-06-23 13:28:19 -0500

    first commit

--- SwEngHomework/SwEngHomework/Commissions/CommissionCalculator.cs
+++ SwEngHomework/SwEngHomework/Commissions/CommissionCalculator.cs
@@ -1,11 +1,85 @@
-﻿namespace SwEngHomework.Commissions
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+namespace SwEngHomework.Commissions
 {
     public class CommissionCalculator : ICommissionCalculator
     {
         public IDictionary<string, double> CalculateCommissionsByAdvisor(string jsonInput)
-        {
+        { 
+            var jsonObj = JObject.Parse(jsonInput);
+            var advisors = ((JArray)jsonObj["advisors"]).ToObject<List<Advisor>>();
+            var accounts = ((JArray)jsonObj["accounts"]).ToObject<List<Account>>();
+
+            var output = new Dictionary<string, double>();
+
+            var advisorAccounts = advisors.GroupJoin(accounts,
+                adv => adv.Name,
+                acct => acct.Advisor,
+                (advisor, account) => new
+                {
+                    advisor.Name,
+                    advisor.Level,
+                    Accounts = account.Select(ad => ad).ToList(),
+                }).ToList()
+                .Select(p => new
+                {
+                    p.Name,
+                    CommissionTotal = Math.Round(p.Accounts.Sum(x => CalculateAdvisorCommission(p.Level, x.PresentValue)),2)
+                }).ToList();
+
+            output = advisorAccounts.ToDictionary(x => x.Name, y => y.CommissionTotal);
+            return output;
+
             // TODO: your implementation
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
         }
+
+        public double CalculateAdvisorCommission(string? level, double presentValue)
+        {
+            switch (level)
+            {
+                case "Senior":
+                    return GetBasisPointsMultiplier(presentValue) * 1;
+
+                case "Experienced":
+                    return GetBasisPointsMultiplier(presentValue) * .5;
+
+                case "Junior":
+                    return GetBasisPointsMultiplier(presentValue) * .25;
+
+                default: return 0;
+            }
+        }
+
+        public double GetBasisPointsMultiplier(double presentValue)
+        {
+            if (presentValue < 50000)
+                return (presentValue * .0005);
+
+            if (presentValue >= 50000 && presentValue < 100000)
+                return (presentValue * .0006);
+
+            if (presentValue >= 100000)
+                return (presentValue * .0007);
+
+            return 0;
+        }
+    }
+    
+    public class Advisor
+    {
+        public string? Name { get; set; }
+        public string? Level { get; set; }
+    }
+
+    public class Account
+    {
+        public string? Advisor { get; set; }
+        public double PresentValue { get; set; }
     }
 }
--- SwEngHomework/SwEngHomework/DescriptiveStatistics/StatsCalculator.cs
+++ SwEngHomework/SwEngHomework/DescriptiveStatistics/StatsCalculator.cs
@@ -1,11 +1,63 @@
-﻿namespace SwEngHomework.DescriptiveStatistics
+﻿using System.Text.RegularExpressions;
+
+namespace SwEngHomework.DescriptiveStatistics
 {
     public class StatsCalculator : IStatsCalculator
     {
         public Stats Calculate(string semicolonDelimitedContributions)
         {
+            var stats = new Stats { Average = 0, Median = 0, Range = 0 };
+
+            //string[] contributionValues = semicolonDelimitedContributions.Split(";");
+            var contributionList = new List<string>(semicolonDelimitedContributions.Split(';')).ToList();
+
+            var listOfDoubles = new List<double>();
+
+            foreach (string contribution in contributionList)
+            {
+                // TODO: Might be better to do a RegEx here to allow for more flexibility
+                var newString = contribution.Replace("," , string.Empty).Replace("$", string.Empty);
+
+                var isValueAValidDouble = Double.TryParse(newString, out double value);
+
+                if(!isValueAValidDouble)
+                {
+                    break;
+                }
+
+                listOfDoubles.Add(value);
+            }
+
+            if(listOfDoubles.Count == 0 ) { return stats; }
+
+            stats.Average = Math.Round(listOfDoubles.Average(),2);
+            stats.Median = Math.Round(CalculateMedian(listOfDoubles), 2);
+            stats.Range = Math.Round(listOfDoubles.Max() - listOfDoubles.Min(), 2);
+
+
+            return stats;
+
+
             // TODO: your implementation
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+        }
+
+        public double CalculateMedian(List<double> entries)
+        {
+            double median;
+
+            var orderedList = entries.OrderBy(p => p).ToList();
+            if (orderedList.Count % 2 == 0)
+            {
+                var firstValue = orderedList[(orderedList.Count / 2) - 1];
+                var secondValue = orderedList[(orderedList.Count / 2)];
+                median = (firstValue + secondValue) / 2;
+            }
+            else
+            {
+                median = orderedList[(orderedList.Count / 2)]; ;
+            }
+            return median;
         }
     }
 }
